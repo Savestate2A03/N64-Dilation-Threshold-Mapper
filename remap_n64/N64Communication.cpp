f20d94d0de2d5abb464b8849d5dd370f391ae44a
Jademalo <386846+Jademalo@users.noreply.github.com>
2020-02-12 03:09:43 +0000

    Perfect timing, doesn't work though

--- remap_n64/N64Communication.cpp
+++ remap_n64/N64Communication.cpp
@@ -2,7 +2,6 @@
 
 /*
  Copyright (c) 2009 Andrew Brown
-
  Permission is hereby granted, free of charge, to any person
  obtaining a copy of this software and associated documentation
  files (the "Software"), to deal in the Software without
@@ -11,10 +10,8 @@
  copies of the Software, and to permit persons to whom the
  Software is furnished to do so, subject to the following
  conditions:
-
  The above copyright notice and this permission notice shall be
  included in all copies or substantial portions of the Software.
-
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -27,6 +24,7 @@
 
 #include <Arduino.h>
 #include "N64Communication.h"
+
 
 #define NOP asm volatile ("nop")
 #define NOP5 asm volatile ("nop\nnop\nnop\nnop\nnop\n")
@@ -66,59 +64,92 @@ static void n64_send(unsigned char *buffer, char length, bool wide_stop)
     // I use manually constructed for-loops out of gotos so I have more control
     // over the outputted assembly. I can insert nops where it was impossible
     // with a for loop
-    N64_LOW;
     
     asm volatile (";Starting outer for loop");
-  for(; length > 0; length = length - 1){
-     for( char bits = 8; bits > 0; bits = bits - 1){
-               // Starting a bit, set the line low
-            N64_LOW; // 1 op, 2 cycles
+outer_loop:
+    {
+        asm volatile (";Starting inner for loop");
+        bits=8;
+inner_loop:
+        {
+          N64_HIGH;
+            // Starting a bit, set the line low
+            asm volatile (";Setting line to low");
 
+            asm volatile (";branching");
             if (*buffer >> 7) {
+              N64_LOW; // 1 op, 2 cycles
                 asm volatile (";Bit is a 1");
                 // 1 bit
                 // remain low for 1us, then go high for 3us
                 // nop block 1
-           
-
+                NOP; NOP; NOP;
+                asm volatile (";Setting line to high");
                 N64_HIGH;
 
                 // nop block 2
                 // we'll wait only 2us to sync up with both conditions
                 // at the bottom of the if statement
-                NOP5; NOP5; NOP;
+                NOP5;
 
             } else {
+              N64_LOW; // 1 op, 2 cycles
                 asm volatile (";Bit is a 0");
                 // 0 bit
                 // remain low for 3us, then go high for 1us
                 // nop block 3
-                NOP5; NOP; NOP; NOP;
+                NOP5; NOP; NOP;
+
                 asm volatile (";Setting line to high");
-                N64_HIGH;
+                //N64_HIGH;
+
                 // wait for 1us
-                //N64_LOW;
                 asm volatile ("; end of conditional branch, need to wait 1us more before next bit");
                 
             }
-            
-            *buffer <<= 1;
+            // end of the if, the line is high and needs to remain
+            // high for exactly 16 more cycles, regardless of the previous
+            // branch path
+
+            asm volatile (";finishing inner loop body");
+            --bits;
+            if (bits != 0) {
+                // nop block 4
+                // this block is why a for loop was impossible
+                // rotate bits
+                asm volatile (";rotating out bits");
+                *buffer <<= 1;
+                //N64_HIGH;
+
+                goto inner_loop;
+            } // fall out of inner loop
+            //N64_HIGH;
         }
-    
-    ++buffer;
-
-
+        asm volatile (";continuing outer loop");
+        // In this case: the inner loop exits and the outer loop iterates,
+        // there are /exactly/ 16 cycles taken up by the necessary operations.
+        // So no nops are needed here (that was lucky!)
+        --length;
+        if (length != 0) {
+            ++buffer;
+            goto outer_loop;
+        } // fall out of outer loop
     }
+
     // send a single stop (1) bit
     // nop block 5
+    asm volatile ("nop\nnop\nnop\nnop\n");
+    N64_LOW;
     // wait 1 us, 16 cycles, then raise the line 
     // take another 3 off for the wide_stop check
     // 16-2-3=11
     // nop block 6
-    N64_LOW;
-    NOP; NOP; NOP;
+    NOP; NOP;
     if (wide_stop) {
-    NOP5; NOP; NOP; NOP;
+        asm volatile (";another 1us for extra wide stop bit\n"
+                      "nop\nnop\nnop\nnop\nnop\n"
+                      "nop\nnop\nnop\nnop\nnop\n"  
+                      "nop\nnop\nnop\nnop\n");
     }
 
     N64_HIGH;
@@ -163,7 +194,7 @@ read_loop:
         while (N64_QUERY){}
 
         // wait approx 2us and poll the line
-        NOP5; 
+        NOP5;
         if (N64_QUERY)
             n64_command |= 0x01;
 
@@ -318,8 +349,6 @@ static void n64_command_wait() {
             if (addr == 0xC0) {
                 // rumble = (data != 0);
             }
-            
-            
             /*
             Serial.println("It was 0x03: the write command");
             Serial.print("Addr was 0x");
